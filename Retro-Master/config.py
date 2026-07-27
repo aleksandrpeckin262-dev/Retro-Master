9be71acbe91bcf17286d5f6a0252167f84a851ee
aleksandrpeckin262-dev <aleksandrpeckin262@gmail.com>
2026-07-27 19:42:53 +0300

    Version 1.2
    Был проведен небольшой рефакторинг кода, было изменено имя папки проекта на Retro-Master.

---
+++ Retro-Master/config.py
@@ -0,0 +1,87 @@
+import os
+
+# Настройки GUI
+WINDOW_TITLE = "Retro master"
+WINDOW_GEOMETRY = "400x300"
+COLOR_BG_TEAL = "#008080"
+COLOR_BTN_BG = "#4FC7DB"
+COLOR_BTN_ACTIVE = "#006363"
+FONT_INTERFACE = ("Arial", 13)
+
+# Базовые каталоги 
+ICON_PATH = r'D:\Retro-Master\icon.ico'
+BASE_DIR = os.getcwd()
+PATH_IMAGE_XP = os.path.join(BASE_DIR, "bg_xp.png")
+
+# Пути к файлам
+THEME_RESOURCES = {
+    "win95": {
+        "wallpaper": r"D:\Retro-Master\bacground_now\bacground_now_win95\win95_wallpaper.jpg",                        # рабочий стол
+        "sound_start": r"D:\Retro-Master\sounds_now\sounds_win95\start_win95\start_win95.wav",                        # запуск win95
+        "icon_computer": r"D:\Retro-Master\icons_now\icon_win95\computer_win95\computer_win95.ico",                   # иконка ПК
+        "icon_trash_empty": r"D:\Retro-Master\icons_now\icon_win95\trash_empty_win95\trash_empty_win95.ico",          # иконка пустой корзины
+        "icon_trash_full": r"D:\Retro-Master\icons_now\icon_win95\trash_full_win95\trash_full_win95.ico",             # иконка полной корзины
+        "cursors_dir": r"D:\Retro-Master\cursor_now\win95_cur"                                                        # Папка, где лежат курсоры 95
+    },
+    "win98": {
+        "wallpaper": r"D:\Retro-Master\bacground_now\bacground_now_win98\win98_wallpaper.jpg",                        # рабочий стол
+        "sound_start": r"D:\Retro-Master\sounds_now\sounds_win98\start_win98\start_win98.wav",                        # запуск win98
+        "sound_click": r"D:\Retro-Master\sounds_now\sounds_win98\click_win98\click_win98.wav",                        # звук клика
+        "icon_computer": r"D:\Retro-Master\icons_now\icon_win98\computer_win98\computer_win98.ico",                   # иконка ПК
+        "icon_trash_empty": r"D:\Retro-Master\icons_now\icon_win98\trash_empty_win98\trash_empty_win98.ico",          # иконка пустой корзины
+        "icon_trash_full": r"D:\Retro-Master\icons_now\icon_win98\trash_full_win98\trash_full_win98.ico",             # иконка полной корзины
+        "cursors_dir": r"D:\Retro-Master\cursor_now\win98_cur"                                                        # Папка, где лежат курсоры 98
+    },
+    "winXP": {
+        "wallpaper": r"D:\Retro-Master\bacground_now\bacground_now_winXP\winXP_wallpaper.jpg",                        # рабочий стол
+        "sound_start": r"D:\Retro-Master\sounds_now\sounds_winXP\start_winXP\start_winXP.wav",                        # запуск winXP
+        "icon_computer": r"D:\Retro-Master\icons_now\computer_winXP\computer_winXP.ico",                              # иконка ПК
+        "icon_trash_empty": r"D:\Retro-Master\icons_now\icon_winXP\trash_empty_winXP\trash_empty_winXP.ico",          # иконка пустой корзины
+        "icon_trash_full": r"D:\Retro-Master\icons_now\icon_winXP\trash_full_winXP\trash_full_winXP.ico",             # иконка полной корзины
+        "cursors_dir": r"D:\Retro-Master\cursor_now\winXP_cur"                                                        # Папка, где лежат курсоры XP
+    },
+    "vista": {
+        "wallpaper": r"D:\Retro-Master\bacground_now\bacground_now_winVista\winVista_wallpaper.jpg",                  # рабочий стол
+        "sound_start": r"D:\Retro-Master\sounds_now\sounds_winVista\start_winVista\start_winVista.wav",               # запуск winVista
+        "icon_computer": r"D:\Retro-Master\icons_now\icon_winVista\computer_winVista\computer_winVista.ico",          # иконка ПК
+        "icon_trash_empty": r"D:\Retro-Master\icons_now\icon_winVista\trash_empty_winVista\trash_empty_winVista.ico", # иконка пустой корзины
+        "icon_trash_full": r"D:\Retro-Master\icons_now\icon_winVista\trash_full_winVista\trash_full_winVista.ico",    # иконка полной корзины
+        "cursors_dir": r"D:\Retro-Master\cursor_now\winVista_cur"                                                     # Папка, где лежат курсоры Vista
+    },
+    "win7": {
+        "wallpaper": r"D:\Retro-Master\bacground_now\bacground_now_win7\win7_wallpaper.jpg",                          # рабочий стол
+        "sound_start": r"D:\Retro-Master\sounds_now\sounds_win7\start_win7\start_win7.wav",                           # запуск win7
+        "icon_computer": r"D:\Retro-Master\icons_now\icon_win7\computer_win7\computer_win7.ico",                      # иконка ПК
+        "icon_trash_empty": r"D:\Retro-Master\icons_now\icon_win7\trash_empty_win7\trash_empty_win7.ico",             # иконка пустой корзины
+        "icon_trash_full": r"D:\Retro-Master\icons_now\icon_win7\trash_full_win7\trash_full_win7.ico",                # иконка полной корзины
+        "cursors_dir": r"D:\Retro-Master\cursor_now\win7_cur"                                                         # Папка, где лежат курсоры 7
+    },
+    # Пути для отката
+    "restore": {
+        "wallpaper": r"D:\Retro-Master\bacground_old\default_wallpaper.jpg",                                          # дефолтные обои
+        "sound_close": r"D:\Retro-Master\sounds_now\sounds_win95\close_win95\close_win95.wav",                        # звук выхода 
+        "cursors_old_dir": r"D:\Retro-Master\cursors_old",                                                            # старый курсор
+        "icons_old_dir": r"D:\Retro-Master\icons_old"                                                                 # старые иконки
+    }
+}
+
+# Названия курсоров и системные константы
+CURSOR_NAMES = ["Arrow", "Help", "AppStarting", "Wait", "IBeam"]
+CMD_KILL_EXPLORER = "taskkill /f /im explorer.exe"
+CMD_START_EXPLORER = "start explorer"
+
+SPI_SETDESKWALLPAPER = 20
+SPI_SETCURSORS = 87
+SPI_SETFONTSMOOTHING = 0x004B
+SPI_FLAGS_IMMEDIATE = 0x01 | 0x02
+
+SMOOTHING_OFF_VAL = "0"
+SMOOTHING_OFF_TYPE = 0
+SMOOTHING_ON_VAL = "2"
+SMOOTHING_ON_TYPE = 2
+
+HWND_BROADCAST = 0xFFFF
+WM_SETTINGCHANGE = 0x001A
+
+REG_PATH_COMP_ICON = r"Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}\DefaultIcon"
+REG_PATH_TRASH_ICON = r"Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon"
---
+++ Retro-Master/main.py
@@ -0,0 +1,139 @@
+import os
+import tkinter as tk
+import threading
+import time
+import ctypes
+import config
+from os_manager import WindowsManager
+
+class GUE(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.os_manager = WindowsManager()
+        self.title(config.WINDOW_TITLE)
+        self.geometry(config.WINDOW_GEOMETRY)
+        self.resizable(False, False)
+        
+        if os.path.exists(config.ICON_PATH): 
+            self.iconbitmap(config.ICON_PATH)
+        
+        self.selected_version = tk.StringVar(value="win95")
+        self.label_title = tk.Label(self, text="Select OS Theme:")
+        self.label_title.place(x=20, y=20)
+        
+        versions_list = [
+            ("Windows 95", "win95", 50), 
+            ("Windows 98", "win98", 80), 
+            ("Windows XP", "winXP", 110), 
+            ("Windows Vista", "vista", 140), 
+            ("Windows 7", "win7", 170)
+        ]
+        
+        for text, version_code, y_position in versions_list:
+            radio_btn = tk.Radiobutton(
+                self, text=text, variable=self.selected_version, 
+                value=version_code, command=self.update_app_style
+            )
+            radio_btn.place(x=20, y=y_position)
+            
+        self.all_radios = [widget for widget in self.winfo_children() if isinstance(widget, tk.Radiobutton)]
+        
+        tk.Button(self, text="Start", command=self.apply_theme).place(x=220, y=40, width=140, height=45)
+        tk.Button(self, text="Reset", command=self.restore_theme).place(x=220, y=110, width=140, height=45)
+        tk.Button(self, text="Exit", command=self.destroy).place(x=220, y=180, width=140, height=45)
+        
+        self.all_btns = [widget for widget in self.winfo_children() if isinstance(widget, tk.Button)]
+        self.update_app_style()
+
+    def update_app_style(self):
+        current_version = self.selected_version.get()
+        
+        for button in self.all_btns: 
+            button.configure(image="", compound="none")
+            
+        # Смена GUI окна програмы
+        if current_version == "win95": 
+            app_bg, btn_bg, btn_act, btn_fg, font, text_c, relief, bd = config.COLOR_BG_TEAL, "#D4D0C8", "#B0B0B0", "#000000", ("MS Sans Serif", 11), "#FFFFFF", "raised", 3
+        elif current_version == "win98": 
+            app_bg, btn_bg, btn_act, btn_fg, font, text_c, relief, bd = "#007070", "#D4D0C8", "#B0B0B0", "#000000", ("MS Sans Serif", 11), "#FFFFFF", "raised", 2
+        elif current_version == "winXP": 
+            app_bg, btn_bg, btn_act, btn_fg, font, text_c, relief, bd = "#245DD7", "#5B9BD5", "#41719C", "#FFFFFF", ("Tahoma", 11), "#FFFFFF", "flat", 1
+        elif current_version == "vista": 
+            app_bg, btn_bg, btn_act, btn_fg, font, text_c, relief, bd = "#202020", "#0078D7", "#005A9E", "#FFFFFF", ("Segoe UI", 11), "#FFFFFF", "flat", 0
+        elif current_version == "win7": 
+            app_bg, btn_bg, btn_act, btn_fg, font, text_c, relief, bd = "#0354AD", "#11B7EB", "#0E96C2", "#000000", ("Segoe UI", 11), "#FFFFFF", "raised", 1
+            
+        self.configure(bg=app_bg)
+        self.label_title.configure(bg=app_bg, fg=text_c, font=font)
+        
+        for radio in self.all_radios: 
+            active_bg = "#19398a" if current_version == "winXP" else app_bg
+            radio.configure(bd=0, highlightthickness=0, bg=app_bg, fg=text_c, activebackground=active_bg, activeforeground=text_c, font=font)
+            
+        for button in self.all_btns: 
+            button.configure(bg=btn_bg, fg=btn_fg, activebackground=btn_act, activeforeground=btn_fg, font=font, relief=relief, bd=bd)
+
+    def apply_theme(self):
+        target_version = self.selected_version.get()
+        self.update_idletasks()
+        
+        def run_backend():
+            # Запускаем WinAPI в потоке, чтобы GUI Tkinter не фризился
+            target_font = "MS Sans Serif" if target_version in ["win95", "win98"] else ("Tahoma" if target_version == "winXP" else "Segoe UI")
+            self.os_manager.set_global_font_substitute(target_font)
+            
+            resource_pack = config.THEME_RESOURCES.get(target_version)
+            
+            # Сброс старых ретро-эффектов
+            self.os_manager.set_retro_taskbar_color(enable=False)
+            self.os_manager.set_retro_colors_win32(version_name="default", enable=False)
+            
+            self.os_manager.play_sound_direct(resource_pack.get("sound_start"))
+            self.os_manager.set_wallpaper(resource_pack.get("wallpaper"))
+            self.os_manager.set_all_cursors_direct(resource_pack.get("cursors_dir"), is_reset=False)
+            self.os_manager.set_system_icons_direct(resource_pack.get("icon_computer"), resource_pack.get("icon_trash_empty"), resource_pack.get("icon_trash_full"), is_reset=False)
+            
+            # ClearType отключается для пиксельного текста
+            self.os_manager.set_clear_type(enable=(target_version != "win95" and target_version != "win98"))
+            
+            if target_version in ["win95", "win98"]:
+                self.os_manager.set_retro_taskbar_color(enable=True)
+                self.os_manager.set_retro_colors_win32(version_name=target_version, enable=True)
+                if target_version == "win98": 
+                    self.os_manager.set_explorer_click_sound_direct(resource_pack.get("sound_click"), enable=True)
+            
+            # Броадкаст HWND_BROADCAST принудительно обновляет метрики во всех открытых окнах ОС
+            ctypes.windll.user32.SendMessageW(config.HWND_BROADCAST, config.WM_SETTINGCHANGE, 0, "Registry::String")
+            time.sleep(0.8) # Пауза чтобы ОС успела перерисовать контейнеры до убийства проводника
+            
+            self.os_manager.restart_shell()
+            
+        threading.Thread(target=run_backend, daemon=True).start()
+
+    def restore_theme(self):
+        self.update_idletasks()
+        
+        def run_restore():
+            self.os_manager.set_global_font_substitute(None)
+            resource_pack = config.THEME_RESOURCES["restore"]
+            
+            self.os_manager.play_sound_direct(resource_pack.get("sound_close"))
+            self.os_manager.set_wallpaper(resource_pack.get("wallpaper"))
+            self.os_manager.set_all_cursors_direct(resource_pack.get("cursors_old_dir"), is_reset=True)
+            self.os_manager.set_clear_type(enable=True)
+            self.os_manager.set_explorer_click_sound_direct(None, enable=False)
+            self.os_manager.set_system_icons_direct(None, None, None, is_reset=True)
+            self.os_manager.set_retro_taskbar_color(enable=False)
+            self.os_manager.set_retro_colors_win32(version_name="default", enable=False)
+            
+            ctypes.windll.user32.SendMessageW(config.HWND_BROADCAST, config.WM_SETTINGCHANGE, 0, "Registry::String")
+            time.sleep(0.8)
+            
+            self.os_manager.restart_shell()
+            
+        threading.Thread(target=run_restore, daemon=True).start()
+
+if __name__ == "__main__":
+    app = GUE()
+    app.mainloop()
+
